7ce2879515d425ac5883470f50bdbde34bbf2359
Homer Huang <aclisp@gmail.com>
2013-09-09 22:08:27 +0800

    a

---
+++ BasicJsf/src/java/my/rapid/NewSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.rapid;
+
+//import javax.ejb.Stateful;
+
+/**
+ *
+ * @author Huang
+ */
+//@Stateful
+public class NewSessionBean {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
